2f657938f82151c77072d98875b48a9993abcdb7
FeigMary <feigmary@gmail.com>
2023-05-26 16:47:14 +0300

    task2

---
+++ src/main/java/sem1/Task2.java
@@ -0,0 +1,34 @@
+package sem1;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+// 2) Вывести все простые числа от 1 до 1000
+
+public class Task2 {
+    public static void main(String[] args) {
+        System.out.print("Введите выбранное положительное число от 1 до 1000: ");
+        Scanner Number = new Scanner(System.in);
+        int N = Number.nextInt();
+        Number.close();
+        List<Integer> primes = new ArrayList<>();
+
+        for (int i = 2; i <= N; i++) {
+            boolean Number2 = true;
+
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    Number2 = false;
+                    break;
+                }
+            }
+
+            if (Number2) {
+                primes.add(i);
+            }
+        }
+
+        System.out.println("Простые числа в указанном числе = " + primes);
+    }
+}
